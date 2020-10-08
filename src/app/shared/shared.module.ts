51dd17b1030fc22ebf84f1763f24fe7ef0cbf61d
vtsitlak <vtsitlak@gmail.com>
2020-10-08 11:13:14 +0200

    toolbar and menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 const MATERIAL_MODULES: any[] = [
   MatIconModule,
   MatMenuModule,
   MatToolbarModule,
+  MatButtonModule,
 ];
 
 @NgModule({
