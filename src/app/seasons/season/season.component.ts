0668e7f10eee436a3c1faf14d5bacc6ac6dea489
vtsitlak <vtsitlak@gmail.com>
2020-10-08 21:13:21 +0200

    prepare able and find winner

--- src/app/seasons/season/season.component.ts
+++ src/app/seasons/season/season.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { RaceTable, Driver } from '../../@interfaces/race';
+import { RaceTable, Driver, Race, Result } from '../../@interfaces/race';
+
+export interface RaceDispaySource {
+  date: string;
+  raceName: string;
+  round: string;
+  winner: Driver;
+}
 
 @Component({
   selector: 'app-season',
@@ -10,12 +17,35 @@ export class SeasonComponent implements OnInit {
 
   raceTable: RaceTable;
   champion: Driver;
+  dataSource: RaceDispaySource[] = [];
 
-  constructor() { }
 
   ngOnInit(): void {
     this.raceTable = history.state.raceTable;
     this.champion = history.state.champion;
+
+    this.raceTable.Races.forEach(race => {
+      const dataSource: RaceDispaySource = {
+        date: race.date,
+        raceName: race.raceName,
+        round: race.round,
+        winner: this.raceWinner(race.Results),
+      };
+
+      this.dataSource.push(dataSource);
+    });
+  }
+
+  raceWinner(results: Result[]): Driver {
+
+    const maxPoints = Math.max(...results.map(result => parseInt(result.points, 10)), 0);
+    const maxPointsIndex = results.map(result => result.points).indexOf(maxPoints.toString());
+
+    return results[maxPointsIndex].Driver;
+  }
+
+  isChampion(winner: Driver): boolean {
+    return winner.driverId === this.champion.driverId;
   }
 
 }
